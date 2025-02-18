1aedd34e845d31778e2d3db049d64e4a4b2a7f6a
Inna Ianko <inna.i@mate.academy>
2025-02-18 12:06:31 +0200

    Removed describe

--- tests/createArticle/createArticleWithoutRequiredFields.spec.js
+++ tests/createArticle/createArticleWithoutRequiredFields.spec.js
@@ -4,35 +4,33 @@ import { HomePage } from '../../src/pages/HomePage';
 import { CreateArticlePage } from '../../src/pages/CreateArticlePage';
 import { faker } from '@faker-js/faker';
 
-test.describe('Create an article', () => {
-  let homePage;
-  let createArticlePage;
+let homePage;
+let createArticlePage;
 
-  test.beforeEach(async ({ page }) => {
-    const signUpPage = new SignUpPage(page);
-    homePage = new HomePage(page);
-    createArticlePage = new CreateArticlePage(page);
+test.beforeEach(async ({ page }) => {
+  const signUpPage = new SignUpPage(page);
+  homePage = new HomePage(page);
+  createArticlePage = new CreateArticlePage(page);
 
-    const user = {
-      username: `${faker.person.firstName()}_${faker.person.lastName()}`,
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
+  const user = {
+    username: `${faker.person.firstName()}_${faker.person.lastName()}`,
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
 
-    await signUpPage.open();
-    await signUpPage.fillUsernameField(user.username);
-    await signUpPage.fillEmailField(user.email);
-    await signUpPage.fillPasswordField(user.password);
-    await signUpPage.clickSignUpButton();
-    await homePage.assertYourFeedTabIsVisible();
-  });
+  await signUpPage.open();
+  await signUpPage.fillUsernameField(user.username);
+  await signUpPage.fillEmailField(user.email);
+  await signUpPage.fillPasswordField(user.password);
+  await signUpPage.clickSignUpButton();
+  await homePage.assertYourFeedTabIsVisible();
+});
 
-  test('Creat an article without required fields', async () => {
-    await homePage.clickNewArticleLink();
+test('Creat an article without required fields', async () => {
+  await homePage.clickNewArticleLink();
 
-    await createArticlePage.clickPublishArticleButton();
-    await createArticlePage.assertErrorMessageContainsText(
-      'Article title cannot be empty',
-    );
-  });
+  await createArticlePage.clickPublishArticleButton();
+  await createArticlePage.assertErrorMessageContainsText(
+    'Article title cannot be empty',
+  );
 });
